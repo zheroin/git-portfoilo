39037af02fe5499f053d7590f8e5ec34af3e482f
Clean Code Creature <128571844+zheroin@users.noreply.github.com>
2023-04-27 06:56:10 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -28,7 +28,7 @@ const config = {
     email: 'onceBetter2025@outlook.com',
   },
   resume: {
-    fileUrl: `https://drive.google.com/file/d/1AkWBId_lIsfBEFnZ3tu5Sy0dwwivvvl9/view?usp=sharing`, // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: `https://drive.google.com/file/d/1YFtNsfym5hl-L8Ydna0_xigGhwLEMMAo/view?usp=share_link`, // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
